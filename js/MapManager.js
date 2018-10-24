12e16ce3e0d6f298a65c315bd5433bb19f047d44
Bijantav <36514247+Bijantav@users.noreply.github.com>
2018-10-25 00:15:45 +0200

    Update MapManager.js

--- js/MapManager.js
+++ js/MapManager.js
@@ -6,7 +6,8 @@ var Event = (function($) { return function(properties) {
       this.className = properties.event_type_name.replace(/[^\w]/ig,"-").toLowerCase();
       this.LatLng = [parseFloat(this.properties.latitude),
                      parseFloat(this.properties.longitude)];
-      moment.locale('de')
+      moment.locale('de');
+      moment.updateLocale('de');
       this.startTime = moment(this.properties.start_dt)._d;
       this.endTime = this.properties.end_dt ? moment(this.properties.end_dt)._d : null;
       this.visible = true;
